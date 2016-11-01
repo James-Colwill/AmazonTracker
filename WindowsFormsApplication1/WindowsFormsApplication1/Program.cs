f00560a0c7e1a93b28fbf06756719a8db111acdf
James C <colwill.james@gmail.com>
2016-11-01 10:03:25 -0400

    Update Program.cs

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -19,21 +19,6 @@ namespace WindowsFormsApplication1
             Application.Run(new Form1());
         }
 
-        //driver.Navigate().GoToUrl("https://www.amazon.com/Amazon-Echo-Bluetooth-Speaker-with-WiFi-Alexa/dp/B00X4WHP5E/ref=sr_1_1?ie=UTF8&qid=1477938972&sr=8-1&keywords=echo");
-        //IWebElement temp = driver.FindElement(By.Id("priceblock_ourprice"));
-        //string price = temp.Text;
-
-        //SmtpClient client;
-        //client = new SmtpClient("smtp.trashmail.com");
-        //client.EnableSsl = true;
-        //    //client.UseDefaultCredentials = false;
-        //    //client.Credentials = new NetworkCredential("colwill.james", "WildLlama$");
-        //    System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
-        //message.To.Add("4404799057@vtext.com");
-        //    message.Subject = "Amazon Echo Price";
-        //    message.From = new System.Net.Mail.MailAddress("uxoyeodh@trashmail.com");
-        //    message.Body = price;
-            
-        //    client.Send(message);
+        
     }
 }
